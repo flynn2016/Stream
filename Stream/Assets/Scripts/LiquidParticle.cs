c6edae652785cda12619ff84512b08e5081ba4b4
Fan Ling <troyprag816@gmail.com>
2019-11-17 14:49:50 -0500

    stream1.0

---
+++ Stream/Assets/Scripts/LiquidParticle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidParticle : MonoBehaviour
+{
+    float timer;
+    float timer_destory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 0.2f)
+        {
+            this.GetComponent<CircleCollider2D>().enabled = true;
+        }
+
+        if (this.transform.localPosition.y < -20f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Container")
+        {
+            timer_destory += Time.deltaTime;
+            if(timer>1f)
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Stream/Assets/Scripts/LiquidSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidSpawner : MonoBehaviour
+{
+    public GameObject liquidParticle;
+    public float flowRate;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > flowRate)
+        {
+            Vector3 random_spawn = new Vector3(this.transform.position.x+Random.Range(-0.1f,0.1f), this.transform.position.y, this.transform.position.z);
+            Instantiate(liquidParticle, random_spawn, Quaternion.identity, this.transform);
+            timer = 0;
+        }
+    }
+}
